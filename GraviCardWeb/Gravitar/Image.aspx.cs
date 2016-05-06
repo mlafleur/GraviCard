9472b3a6cc7a2fd9de4c62fb84571925f36d68ee
Marc LaFleur <marc@massivescale.com>
2016-05-06 13:19:40 -0400

    Initial Load

---
+++ GraviCardWeb/Gravitar/Image.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace GraviCardWeb.Gravitar
+{
+    public partial class Image : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var emailAddress = Request.QueryString[0];
+            emailAddress = emailAddress.Trim();
+            emailAddress = emailAddress.ToLower();
+            string hash;
+            using (var md5 = MD5.Create())
+            {
+                byte[] data = md5.ComputeHash(Encoding.Default.GetBytes(emailAddress));
+                StringBuilder sBuilder = new StringBuilder();
+                for (int i = 0; i < data.Length; i++)
+                {
+                    sBuilder.Append(data[i].ToString("x2"));
+                }
+                hash = sBuilder.ToString();
+            }
+
+            Response.ContentType = "application/json"; //JSON Text output
+            Response.Write("https://www.gravatar.com/avatar/" + hash + "?d=blank");
+        }
+    }
+}
